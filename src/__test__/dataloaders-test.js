16191c181421b9bef7fe91f684ab9bb75c7ea0c8
Dan Coates <dan.coates@blueflag.com.au>
2017-12-08 10:25:27 +1100

    Add tests for dataloaders

--- src/__test__/dataloaders-test.js
+++ src/__test__/dataloaders-test.js
@@ -2,6 +2,8 @@
 import test from 'ava';
 import decorateLoaders from '../dataloaders';
 import DataLoader from 'dataloader';
+import sinon from 'sinon';
+import {Map} from 'immutable';
 
 test('decorateLoaders takes an object and applies loaders to functions', async (t: Object): Promise<void> => {
     const loaders = {
@@ -33,3 +35,37 @@ test('decorateLoaders will call decorator function for each loader', async (t: O
 });
 
 
+
+
+test('decorateLoaders will call applier function for each loader', async (t: Object): Promise<void> => {
+
+    const loaders = {
+        loaderModule: {
+            loaderFunction: (ids) => Promise.resolve(ids)
+        }
+    };
+
+    const decoratedLoaders = decorateLoaders({loaders, loaderApplier: (fn, ids) => fn(ids.map(ii => ii + ' ' + 'world'))});
+    const result = await decoratedLoaders.loaderModule.loaderFunction.load('hello');
+
+    t.is('hello world', result);
+});
+
+
+test('decorateLoaders will create loaders that can accept immutable data as ids', async (t: Object): Promise<void> => {
+    const loaderFunction = sinon.spy((ids) => Promise.resolve(ids.map(ii => ii.get('id'))));
+    const loaders = {
+        loaderModule: {
+            loaderFunction: loaderFunction
+        }
+    };
+
+    const decoratedLoaders = decorateLoaders({loaders});
+
+    const result1 = await decoratedLoaders.loaderModule.loaderFunction.load(Map({id: '1234'}));
+    const result2 = await decoratedLoaders.loaderModule.loaderFunction.load(Map({id: '1234'}));
+
+    t.true(loaderFunction.calledOnce);
+    t.is('1234', result1);
+    t.is('1234', result2);
+});
